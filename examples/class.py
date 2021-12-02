ce605819534724149c370a66c85a2aff281a5c9f
drawmoon <1340260725@qq.com>
2021-12-02 16:57:46 +0800

    Update class.py

--- examples/class.py
+++ examples/class.py
@@ -1,107 +1,119 @@
-from typing import overload, Union
+from typing import overload, Union, Iterable
 
 
 # 类的声明
 class Store:
-    name: str
+    # 具有以 __ 前后缀的函数称为魔术函数，例如：__init__, __new__,
 
+    # 初始化函数，构造实例的时候被调用
     def __init__(self, name: str):
-        print("Store init")
-        self.name = name
+        print("Store __init__ method will be called")
 
-    def __call__(self, todo: str):
-        print("Store call")
-        print(todo)
+        # 私有字段
+        self.__name = name
 
+    # 当类实例化时被调用，在 __init__ 函数之前被调用，基类中声明了 __new__ 函数，子类中也必须声明 __new__ 函数
+    # def __new__(cls, name: str):
+    #     print("Store __new__ method will be called")
+    #
+    #     return super(Store, cls).__new__(cls)
+
+    # 使实例可以以调用函数的方式被调用
+    def __call__(self) -> None:
+        print("Store __call__ method will be called")
+
+    # 当实例被转换为字符串时被调用
+    def __str__(self) -> str:
+        return self.__name
+
+    # 当调用 dir() 函数时被调用
+    def __dir__(self) -> Iterable[str]:
+        print("Store __dir__ method will be called")
+        return ["get_name"]
+
+    # 析构函数，销毁实例的时候被调用
     def __del__(self):
-        print("Store del")
+        print("Store __del__ method will be called")
 
+    # 公共函数
     def get_name(self) -> str:
-        return self.name
+        return self.__name
+
+    # 受保护的函数
+    def _get_style(self) -> str:
+        pass
+
+    # 私有函数
+    def __get_owner(self) -> str:
+        pass
 
 
-store = Store("Game Store")
-store("run")
-store_name = store.get_name()
-print(store_name)
+# 构造实例
+store = Store("Game World")
+# 调用 __call__ 函数
+store()
+# 调用 __str__ 函数
+str(store)
+# 调用 __dir__ 函数，获取实例的函数
+dir(store)
 
 
-# 类的继承与函数重写
+# 类的继承
 class PhysicalStore(Store):
+    # 初始化函数重写
+    def __init__(self, name: str, description: str):
+        super(PhysicalStore, self).__init__(name)
+        self.description = description
+
+    # 函数重写
     def get_name(self) -> str:
-        print("PhysicalStore get_name")
+        print("PhysicalStore get_name method will be called")
         return super().get_name()
 
 
-store = PhysicalStore("Game World")
-store_name = store.get_name()
-print(store_name)
-
-
-# 类的继承之构造函数
-class CloudStore(Store):
-    tag: str
-
-    def __init__(self, name: str, tag: str):
-        super(CloudStore, self).__init__(name)
-        self.tag = tag
-
-
-store = CloudStore("Steam", "lib")
-print(store.tag)
-
-
 # 函数重载
-class UserService:
+class UserAccessor:
     @overload
-    def get(self, uid: int): ...
+    def get_phone(self, uid: int) -> str: ...
 
     @overload
-    def get(self, name: str): ...
+    def get_phone(self, name: str) -> str: ...
 
-    def get(self, u: Union[int, str]):
-        if type(u) == int:
-            print(f"User id is {u}")
-        else:
-            print(f"User name is {u}")
+    def get_phone(self, option: Union[int, str]) -> str:
+        return ''
 
 
-user_service = UserService()
-user_service.get(1)
-user_service.get("admin")
+user_accessor = UserAccessor()
+user_accessor.get_phone(1)
+user_accessor.get_phone("George")
 
 
-# 判断或获取对象中指定的属性
-class SomeClass:
-    word = "Hello World!"
+# 判断或获取对象中指定的成员
+class People:
+    name = "Pikachu"
 
-    def hi(self):
-        print(self.word)
-
-    def __init__(self):
-        print(hasattr(self, "word"))
-        print(hasattr(self, "hi"))
-
-        print(getattr(self, "word"))
-        print(getattr(self, "hi"))
+    def jog(self) -> None:
+        pass
 
 
-some_class = SomeClass()
+people = People()
+print(hasattr(people, "name"), hasattr(people, "jog"))
+print(getattr(people, "name"), getattr(people, "jog"))
 
 
-# 在父类中调用子类的方法
+# 在基类中调用子类的成员
 class Foo:
-    def process(self, name: str):
+    def __call__(self, member: str) -> None:
         try:
-            getattr(self, name)()
+            getattr(self, member)()
         except AttributeError:
-            print(f"Notfound attr {name}")
+            print(f"Not Found attr {member}")
 
 
 class Bar(Foo):
     def tell(self):
-        print("Call tell fn")
+        print("tell method will be called")
 
 
 bar = Bar()
-bar.process("tell")
+bar("tell")
